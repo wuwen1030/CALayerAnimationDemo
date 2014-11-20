4412b27bb067143e6a22fe4938b6a2e99939655c
XiaBin <xiabin@tuniu.com>
2014-11-20 15:07:29 +0800

    上传代码

---
+++ LADemo/LADemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LADemo
+//
+//  Created by Ben on 14/11/17.
+//  Copyright (c) 2014年 XTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LADemo/LADemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LADemo
+//
+//  Created by Ben on 14/11/17.
+//  Copyright (c) 2014年 XTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
